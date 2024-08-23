9c158ac725dc1c58405d771d91d01ca1cade09ef
Saik Chan <maraohnonpon@hotmail.com>
2024-08-23 22:04:05 +0700

    Initial Commit

---
+++ AnimalCards/AnimalCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalCardsApp.swift
+//  AnimalCards
+//
+//  Created by SaikChan on 23/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimalCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimalCards/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AnimalCards
+//
+//  Created by SaikChan on 23/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
